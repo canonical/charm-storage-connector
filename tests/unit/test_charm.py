d4066ed0e6a86fe14d9a18ae383461975e8cd6a8
camille.rodriguez <camille.rodriguez@canonical.com>
2020-08-14 14:16:03 -0500

    unittests and lint

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -6,16 +6,16 @@ import shutil
 import subprocess
 import tempfile
 import unittest
-from unittest.mock import create_autospec, Mock, patch
 from pathlib import Path
+from unittest.mock import Mock, create_autospec, patch
 
 from charm import CharmIscsiConnectorCharm
-from src import charm
 
 from ops.framework import EventBase
 from ops.testing import Harness
 
-import src.utils as utils
+from src import charm
+
 
 class TestCharm(unittest.TestCase):
     """Charm Unit Tests."""
@@ -23,47 +23,55 @@ class TestCharm(unittest.TestCase):
     subprocess_mock = create_autospec(subprocess.check_call, return_value='True')
     subprocess.check_call = subprocess_mock
 
-    
-
     def setUp(self):
         """Test setup."""
         self.tempdir = tempfile.mkdtemp()
         self.harness = Harness(CharmIscsiConnectorCharm)
         self.harness.set_leader(is_leader=True)
-        
+        self.harness.begin()
+
     def tearDown(self):
         """Remove testing artifacts."""
         shutil.rmtree(self.tempdir)
 
     def test__init__works_without_a_hitch(self):
         """Test init."""
-        self.harness.begin()
 
     def test_abort_if_host_is_container(self):
-        self.harness.begin()
-        charm.utils.is_container = Mock(return_value = True)
+        """Test if charm stops when deployed on a container."""
+        charm.utils.is_container = Mock(return_value=True)
         self.assertFalse(self.harness.charm.store.installed)
 
-
-    @patch("Path", new=tempfile.mkdtemps())
-    def test_on_install(self):
+    @patch("charm.CharmIscsiConnectorCharm._iscsi_initiator")
+    @patch("charm.utils.is_container")
+    @patch("charm.CharmIscsiConnectorCharm.MULTIPATH_CONF")
+    @patch("charm.CharmIscsiConnectorCharm.MULTIPATH_CONF_PATH")
+    @patch("charm.CharmIscsiConnectorCharm.MULTIPATH_CONF_DIR")
+    @patch("charm.CharmIscsiConnectorCharm.ISCSI_INITIATOR_NAME")
+    @patch("charm.CharmIscsiConnectorCharm.ISCSI_CONF")
+    @patch("charm.CharmIscsiConnectorCharm.ISCSI_CONF_PATH")
+    def test_on_install(self, iscsi_conf_path, iscsi_conf, iscsi_initiator_name,
+                        multipath_conf_dir, multipath_conf_path, multipath_conf,
+                        is_container, iscsi_initiator):
         """Test installation."""
-        charm.utils.is_container = Mock(return_value = False)
-
-        self.harness.begin()
-        self.harness.charm.ISCSI_CONF_PATH = Path(tempfile.mkdtemp())
-        self.harness.charm.MULTIPATH_CONF_DIR = Path(tempfile.mkdtemp())
-        
+        is_container.return_value = False
+        iscsi_conf_path.return_value = Path(tempfile.mkdtemp())
+        iscsi_conf.return_value = iscsi_conf_path / 'iscsid.conf'
+        iscsi_initiator_name.return_value = iscsi_conf / 'initiatorname.iscsi'
+        multipath_conf_dir.return_value = Path(tempfile.mkdtemp())
+        multipath_conf_path.return_value = multipath_conf_dir / 'conf.d'
+        multipath_conf.return_value = multipath_conf_path / 'multipath.conf'
+        iscsi_initiator.return_value = None
 
         self.assertFalse(self.harness.charm.store.installed)
         self.harness.charm.on.install.emit()
 
-        self.assertTrue(os.path.isfile(self.harness.charm.ISCSI_CONF))
+        self.assertTrue(os.path.exists(self.harness.charm.ISCSI_CONF))
+        self.assertTrue(os.path.exists(self.harness.charm.MULTIPATH_CONF))
         self.assertTrue(self.harness.charm.store.installed)
 
     def test_on_start(self):
         """Test on start hook."""
-        self.harness.begin()
         self.assertFalse(self.harness.charm.store.started)
         self.harness.charm.on.start.emit()
         # event deferred as charm not configured yet
@@ -75,14 +83,12 @@ class TestCharm(unittest.TestCase):
 
     def test_on_restart_iscsi_services_action(self):
         """Test on restart action."""
-        self.harness.begin()
         action_event = FakeActionEvent()
         self.harness.charm.on_restart_iscsi_services_action(action_event)
         self.assertEqual(action_event.results['success'], 'True')
 
     def test_on_reload_multipathd_service_action(self):
         """Test on reload action."""
-        self.harness.begin()
         action_event = FakeActionEvent()
         self.harness.charm.on_reload_multipathd_service_action(action_event)
         self.assertEqual(action_event.results['success'], 'True')
